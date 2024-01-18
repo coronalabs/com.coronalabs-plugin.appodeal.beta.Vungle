38de6a1f23b16a9aa431be8c881193f46876c16a
Scott Harrison <scottrules44@gmail.com>
2024-01-18 15:29:25 -0600

    Fixes

--- plugins/2020.3569/iphone-sim/metadata.lua
+++ plugins/2020.3569/iphone-sim/metadata.lua
@@ -3,8 +3,8 @@ local metadata =
 	plugin =
 	{
 		format = 'staticLibrary',
-		staticLibs = { 'APDSmaatoAdapter', },
-		frameworks = { 'OMSDK_Smaato', 'SmaatoSDKBanner', 'SmaatoSDKCore', 'SmaatoSDKInterstitial', 'SmaatoSDKOpenMeasurement', 'SmaatoSDKRewardedAds', 'SmaatoSDKRichMedia', 'SmaatoSDKVideo', },
+		staticLibs = { 'APDVungleAdapter', },
+		frameworks = { 'VungleAdsSDK' },
 		frameworksOptional = {},
 	},
 }
